04b7521c05e20edb3128d763f8edaf2a5b7e89ec
evgeniy <you@example.com>
2021-11-17 15:50:16 +0500

    learning 8

--- practice/7 - script/project/js/script.js
+++ practice/7 - script/project/js/script.js
@@ -24,3 +24,20 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelector('.promr__adv img');
+adv.forEach(element => { element.remove(); });
+
+const poster = document.querySelector('.promo__bg');
+const genre = poster.querySelector('.promo__genre'); 
+
+genre.textContent = "Комедия";
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `<li class = "">${i + 1} ${film}<div class="delete"></div></li>`;
+});
