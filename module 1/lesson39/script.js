6457374635466b0583792bb7e9e922988c4c7aef
evgeniy <you@example.com>
2021-12-02 22:19:45 +0500

    lesson 40

---
+++ module 1/lesson39/script.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('.btn');
+let timerId;
+i = 0;
+
+function myAnimation(){
+  const elem = document.querySelector('.box');
+  let pos = 0;
+
+  const id = setInterval(frame, 10);
+  function frame(){
+    if (pos == 3000) {
+      clearInterval(id);
+    }else{
+      pos++;
+      elem.style.top  = pos + "px";
+      elem.style.left = pos + "px";
+    }
+  }
+}
+btn.addEventListener('click', myAnimation);
+
+// рекурсивный setTimeout
+let id = setTimeout(function log(){
+  console.log('hello');
+  id = setTimeout(log, 500);
+}, 500);
+
