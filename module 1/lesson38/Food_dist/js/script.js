bc4fff06a9adf4e3fcc225ec9f3a20a3c8f91d00
evgeniy <you@example.com>
2021-12-02 13:52:26 +0500

    lesson 38

--- module 1/lesson38/Food_dist/js/script.js
+++ module 1/lesson38/Food_dist/js/script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function hideTabContent() {
     tabsContent.forEach(item => {
-      item.style.display = none;
+      // item.style.display = none;
+      item.classList.add("hide");
+      item.classList.remove("show","fade");
+
     })
 
     tabs.forEach(tab => {
@@ -14,7 +17,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function showTabContent(i = 0) {
-    tabsContent[i].style.display = 'block';
+    // tabsContent[i].style.display = 'block';
+    tabsContent[i].classList.add("show", 'fade');
+    tabsContent[i].classList.remove("hide");
     tabs[i].classList.add('tabheader__item_active');
   }
 
