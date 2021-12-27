3d540f9a952b45136bf09841520a0779957ed98e
Evgeniy Saburov <evs.zlat@gmail.com>
2021-12-27 15:56:31 +0500

    lesson78

--- module2/lesson78/script.js
+++ module2/lesson78/script.js
@@ -4,3 +4,23 @@ import $ from 'jquery';
 
 $('#btn');
 _();
+
+$(document).ready(
+  
+  $(".list-item:first").hover(function(){
+    $(this).toggleClass('.active');
+  });
+
+  $('.list-item:eq(2)').on('click', function(){
+    $('.image:even').fadeToggle('slow');
+  });
+
+  $('.list-item:eq(5)').on('click', function(){
+    $('.image:add').animate({
+      opacity:'toggle',
+      height:'toggle',
+    });
+  });
+
+
+);
